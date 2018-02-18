5e6713def629d9cf81a99faf37af480d581e6258
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2018-02-18 23:56:49 +1100

    Starting type-erasure...

--- main.cpp
+++ main.cpp
@@ -5,20 +5,40 @@
 #include <boost/bind.hpp>
 #include <boost/functional/factory.hpp>
 #include <boost/function.hpp>
+#include <boost/variant.hpp>
 
 #include <map>
 #include <utility>
 
-template <class AbstractProduct, typename IdentifierType, typename ProductCreator>
+struct Arity {
+    virtual ~Arity() = default;
+};
+
+struct Nullary : Arity {};
+
+struct Unary : Arity {
+    Unary(double) {}
+};
+
+/*
+template <typename Callable, typename ReturnType, typename... Args>
+struct Impl<Callable, ReturnType(Callable::*)(Args...)> : Concept {
+  */
+
+template <class AbstractProduct, typename IdentifierType>
 class Factory
 {
+    using signatures = boost::mpl::vector<>;
 public:
-    bool Register(const IdentifierType& id, ProductCreator creator) {
-        return associations_.emplace(id, creator).second;
+    template <IdentifierType ID, typename ProductCreator>
+    bool Register(ProductCreator creator) {
+        // typename boost::mpl::push_back<signatures, ProductCreator>::type;
+        variant_handler
+        // return associations_.emplace(id, creator).second;
     }
 
     bool Unregister(const IdentifierType& id) {
-        return associations_.erase(id) == 1;
+        // return associations_.erase(id) == 1;
     }
 
     template <typename... Arguments>
@@ -31,29 +51,39 @@ public:
     }
 
 private:
-    std::map<IdentifierType, ProductCreator> associations_;
+    template <typename... Signatures>
+    using multifunction = boost::type_erasure::any< boost::mpl::vector< boost::type_erasure::copy_constructible<>,
+    boost::type_erasure::typeid_<>, boost::type_erasure::relaxed, boost::type_erasure::callable<Signatures>... > >;
+
+    using variant_type = boost::variant<int, double, std::string> ;
+
+    using function_type = multifunction<Arity*(), Arity*(int)>;
+
+    class variant_handler
+    {
+    public:
+        void handle(IdentifierType id, const variant_type& arg)
+        {
+            boost::apply_visitor(impl, arg);
+        }
+    };
+
+    struct dispatcher : boost::static_visitor<void>
+    {
+        // used for the leaves
+        template <IdentifierType ID, typename... Args>
+        void operator()(Args... args) { f(args...); }
+        // For a vector, we recursively operate on the elements
+        function_type f;
+    };
+
+    std::map<IdentifierType, dispatcher> associations_;
 };
 
-namespace te = boost::type_erasure;
-template <typename... Signatures>
-using multifunction = te::any< boost::mpl::vector< te::copy_constructible<>,
-te::typeid_<>, te::relaxed, te::callable<Signatures>... > >;
-
-struct Arity {
-    virtual ~Arity() = default;
-};
-
-struct Nullary : Arity {};
-
-struct Unary : Arity {
-    Unary(double) {}
-};
-
-using MultiCtors = multifunction<Arity *(), Arity *(double)>;
 
 int main(void)
 {
-    Factory<Arity*, int, boost::function<Arity*()>> factory;
+    Factory<Arity*, int> factory;
     factory.Register(0, boost::factory<Nullary *>() );
     // MultiCtors nullaryFactory = boost::bind( boost::factory<Nullary *>() );
     // MultiCtors nullaryLambda = [](){ return new Nullary(); };
