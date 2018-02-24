2857524a4c940541005a5e1ffff91e2633b2c27f
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2018-02-24 21:54:04 +1100

    Move variadic product creators to class template...

--- main.cpp
+++ main.cpp
@@ -10,30 +10,20 @@
 #include <map>
 #include <utility>
 
-struct Arity {
-    virtual ~Arity() = default;
-};
-
-struct Nullary : Arity {};
-
-struct Unary : Arity {
-    Unary(double) {}
-};
-
 /*
 template <typename Callable, typename ReturnType, typename... Args>
 struct Impl<Callable, ReturnType(Callable::*)(Args...)> : Concept {
   */
 
-template <class AbstractProduct, typename IdentifierType>
+template <class AbstractProduct, typename IdentifierType, typename... ProductCreators>
 class Factory
 {
-    using signatures = boost::mpl::vector<>;
+    using multifunction = boost::type_erasure::any< boost::mpl::vector< boost::type_erasure::copy_constructible<>,
+    boost::type_erasure::typeid_<>, boost::type_erasure::relaxed, boost::type_erasure::callable<ProductCreators>... > >;
+
 public:
-    template <IdentifierType ID, typename ProductCreator>
-    bool Register(ProductCreator creator) {
-        // typename boost::mpl::push_back<signatures, ProductCreator>::type;
-        variant_handler
+    template <typename ProductCreator>
+    bool Register(IdentifierType id, ProductCreator creator) {
         // return associations_.emplace(id, creator).second;
     }
 
@@ -51,13 +41,7 @@ public:
     }
 
 private:
-    template <typename... Signatures>
-    using multifunction = boost::type_erasure::any< boost::mpl::vector< boost::type_erasure::copy_constructible<>,
-    boost::type_erasure::typeid_<>, boost::type_erasure::relaxed, boost::type_erasure::callable<Signatures>... > >;
-
     using variant_type = boost::variant<int, double, std::string> ;
-
-    using function_type = multifunction<Arity*(), Arity*(int)>;
 
     class variant_handler
     {
@@ -74,16 +58,26 @@ private:
         template <IdentifierType ID, typename... Args>
         void operator()(Args... args) { f(args...); }
         // For a vector, we recursively operate on the elements
-        function_type f;
+        multifunction f;
     };
 
     std::map<IdentifierType, dispatcher> associations_;
 };
 
+struct Arity {
+    virtual ~Arity() = default;
+};
+
+struct Nullary : Arity {};
+
+struct Unary : Arity {
+    Unary(double) {}
+};
+
 
 int main(void)
 {
-    Factory<Arity*, int> factory;
+    Factory<Arity*, int, Nullary*(), Unary*(double)> factory;
     factory.Register(0, boost::factory<Nullary *>() );
     // MultiCtors nullaryFactory = boost::bind( boost::factory<Nullary *>() );
     // MultiCtors nullaryLambda = [](){ return new Nullary(); };
